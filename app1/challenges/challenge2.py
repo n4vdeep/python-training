3c49d508b870b0bf1e639513564df9f102bcf9d7
Nav Dhamu <n4vdeep@gmail.com>
2022-11-07 21:01:51 +0000

    day 2: challenge2 - working with while, methods and prints

---
+++ app1/challenges/challenge2.py
@@ -0,0 +1,22 @@
+# SOLUTION
+name = input("What is your name? ")
+print(name.capitalize())
+
+name_prompt = "Enter your name: "
+user_names = []
+while True:
+    name = input(name_prompt)
+    capitalise_first = name.capitalize()
+    user_names.append(name)
+    print(user_names)
+
+name = input("What is your name? ")
+while True:
+    print(name.capitalize())
+
+name_prompt = "Enter your name: "
+
+while True:
+    name = input(name_prompt)
+    capitalise_first = name.capitalize()
+    print(capitalise_first)
