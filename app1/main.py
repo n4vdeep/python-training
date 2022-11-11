8929c32147c4771881bd95bbe17032f8c40f908d
Nav Dhamu <n4vdeep@gmail.com>
2022-11-11 13:25:46 +0000

    day 4: type conversion and list indexing

--- app1/main.py
+++ app1/main.py
@@ -1,9 +1,9 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show or exit: ")
+    user_action = input("Type add, show, edit or exit: ")
     user_action = user_action.strip()
-    
+
     match user_action:
         case 'add':
             todo = input("Enter a todo: ")
@@ -11,5 +11,10 @@ while True:
         case 'show':
             for item in todos:
                 print(item)
+        case 'edit':
+            number = int(input("Enter the number of the item you want to edit: "))
+            number -= 1
+            new_item = input("Enter the item you want: ")
+            todos[number] = new_item
         case 'exit':
             break
---
+++ app1/sandbox/nested_for.py
@@ -0,0 +1,11 @@
+# Delivering parcels
+# Deliver parcels to houses but avoid the rocks
+
+for x in range(4):
+    for i in range(4):
+        for j in range(1):
+            print(j, "move")
+            print(j, "move")
+        print(i, "delivered")
+    print(x, "move")
+    print(x, "turn")
