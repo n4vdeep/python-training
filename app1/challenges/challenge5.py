5dec1731c2b9ef3840e1f380d5bb0ac5b1a72570
Nav Dhamu <n4vdeep@gmail.com>
2022-11-20 23:51:14 +0000

    day 6: text files, open, read, write

--- app1/challenges/challenge5.py
+++ app1/challenges/challenge5.py
@@ -8,3 +8,19 @@ user_action = int(input("Enter the index of the IP you want: "))
 user_action -= 1
 print(f"You chose {ips[user_action]}")
 
+menu = ["pasta", "pizza", "salad"]
+
+user_choice = int(input("Enter the index of the item: "))
+
+message = f"You chose {menu[user_choice]}."
+print(message)
+
+menu = ["pasta", "pizza", "salad"]
+
+for i, j in enumerate(menu):
+    print(f"{i}.{j}")
+
+menu = ["pasta", "pizza", "salad"]
+
+for i, j in enumerate(menu):
+    print(f"{i}.{j}")
--- app1/main.py
+++ app1/main.py
@@ -1,14 +1,26 @@
-todos = []
-
 while True:
     user_action = input("Type add, edit, or complete. To see your Todos type show, or exit to close app: ")
     user_action = user_action.strip()
 
     match user_action:
         case 'add':
-            todo = input("Enter a todo: ")
+            todo = input("Enter a todo: ") + "\n"
+
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
+
             todos.append(todo)
+
+            file = open('todos.txt', 'w')
+            file.writelines(todos)
+            file.close()
+
         case 'show':
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
+
             for index, item in enumerate(todos):
                 row = f"{index + 1}-{item}"
                 print(row)
