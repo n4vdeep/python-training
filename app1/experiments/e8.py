d7c4b1c91d73d0748708eccd3070b73f940f2ccc
Nav Dhamu <n4vdeep@gmail.com>
2022-11-29 17:29:39 +0000

    day 8: challenge 8 -  heads or tails challenge

--- app1/experiments/e8.py
+++ app1/experiments/e8.py
@@ -1,4 +1,10 @@
-# Default mode is r
-with open("../files/doc.txt", "r") as file:
+# # Default mode is r
+# with open("../files/doc.txt", "r") as file:
+#     output = file.read()
+#     print(output)
+
+
+with open("../files/test_file.txt", 'r') as file:
     output = file.read()
     print(output)
+    print(len(output))
